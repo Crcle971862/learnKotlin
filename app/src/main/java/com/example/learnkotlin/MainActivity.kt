108679454181298b4ec25fa0025386c3f0d0af0c
manish chaurasiya <139817831+Crcle971862@users.noreply.github.com>
2026-01-21 18:01:31 +0530

    switch

--- app/src/main/java/com/example/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.learnkotlin
 
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,12 +9,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,7 +46,16 @@ class MainActivity : ComponentActivity() {
                                 ) {
                                     Icon(Icons.Filled.Menu, contentDescription ="Home")
                                 }
-                            }
+                            },
+                            actions = {
+                                IconButton(onClick = {}) {
+                                    Icon(Icons.Filled.Notifications, contentDescription = "notification")
+                                }
+                            },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = Color.Yellow,
+                                navigationIconContentColor = Color.Green
+                            )
                         )
 
                     }
